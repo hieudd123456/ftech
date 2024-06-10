a1718be1c9cdd6f5b4def2522494c531fd7ded79
hieudd123456 <130424877+hieudd123456@users.noreply.github.com>
2024-06-10 16:38:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const { Client } = require('pg');
+
+const DATABASE_HOST='ep-broad-mud-a1d8bq9s.ap-southeast-1.pg.koyeb.app';
+const DATABASE_USER='ftechadmin';
+const DATABASE_PASSWORD='LNZix9TwBkQ3';
+const DATABASE_NAME='koyebdb';
+
+const port = process.env.PORT || 3000;
+var client = new Client({
+	user: DATABASE_USER,
+	password: DATABASE_PASSWORD,
+	host: DATABASE_HOST,
+	port: 5432,
+	database: DATABASE_NAME,
+});
+
+client.connect().then(() => {
+		console.log('Connected to PostgreSQL database');
+	}).catch((err) => {
+		console.error('Error connecting to PostgreSQL database', err);
+	});
 
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello, world!: port running: '+port ,
+    postgres:client
   })
 })
 
