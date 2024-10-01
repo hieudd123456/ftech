7b021c05f8ae9192bfd3d4e84ce259101254151c
hieudd123456 <130424877+hieudd123456@users.noreply.github.com>
2024-10-01 17:04:16 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const fs = require('fs')
 const multer = require('multer');
 const path = require('path');
 const app = express();
-const { Client } = require('pg');
-const { Pool } = require('pg');
+// const { Client } = require('pg');
+// const { Pool } = require('pg');
 //import postgres from 'postgres'
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
+const { MongoClient, ServerApiVersion } = require('mongodb');
 // Cấu hình multer để lưu ảnh
 const storage = multer.memoryStorage(); // Lưu ảnh vào bộ nhớ RAM
 const upload = multer({ storage: storage });
@@ -152,19 +153,19 @@ app.get('/data', (req, res) => {
 	})
 })
 
-app.get('/datadate', (req, res) => {
-	let machineserial = req.query.machineserial ;
-	let fromDate = req.query.fromdate ;
+// app.get('/datadate', (req, res) => {
+// 	let machineserial = req.query.machineserial ;
+// 	let fromDate = req.query.fromdate ;
     	
-	if (!machineserial) {
-    		machineserial="5555";
-  	}
-	let fdate = toTimestamp(fromDate) || new Date();
+// 	if (!machineserial) {
+//     		machineserial="5555";
+//   	}
+// 	let fdate = toTimestamp(fromDate) || new Date();
 
- 	readDatabyDate(machineserial,toTimestamp(fdate), (data)=>{
-		res.status(201).json(data);
-	})
-})
+//  	readDatabyDate(machineserial,toTimestamp(fdate), (data)=>{
+// 		res.status(201).json(data);
+// 	})
+// })
 /**
  * Lấy danh sách các bảng
  */
@@ -181,18 +182,18 @@ app.get('/listtable', (req, res) => {
 });
 
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^  API: THEM DU LIEU   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-app.get('/insertdata', (req, res) => {
-	 console.log("request",req.query);
-	let machineserial = req.query.machineserial ;
-    	let temperature = parseFloat(req.query.temperature) ;
-    	let humidity 	= parseFloat(req.query.humidity) ; 
-	if (!machineserial || typeof temperature !== 'number' || typeof humidity !== 'number') {
-    		return res.status(400).json({ error: 'machineserial, Temperature and humidity must be numbers' });
-  	}
-	insertData(machineserial,temperature, humidity,function(isSuccess){
-		res.status(201).json(isSuccess);
-	});
-})
+// app.get('/insertdata', (req, res) => {
+// 	 console.log("request",req.query);
+// 	let machineserial = req.query.machineserial ;
+//     	let temperature = parseFloat(req.query.temperature) ;
+//     	let humidity 	= parseFloat(req.query.humidity) ; 
+// 	if (!machineserial || typeof temperature !== 'number' || typeof humidity !== 'number') {
+//     		return res.status(400).json({ error: 'machineserial, Temperature and humidity must be numbers' });
+//   	}
+// 	insertData(machineserial,temperature, humidity,function(isSuccess){
+// 		res.status(201).json(isSuccess);
+// 	});
+// })
 
 app.get('/index', (req, res) => {
     res.sendFile(path.join(__dirname+'/public/index.html'));
@@ -267,3 +268,144 @@ server.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
 //});
+//-------------------------
+
+
+var password = encodeURIComponent("Hieu%230355185363");
+const uri = `mongodb+srv://hieunguyen00678:Hieu%230355185363@ftechcluster.tl1vi6e.mongodb.net/?appName=ftechcluster`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri);
+var database =null;
+var sensor_table =null;
+var IsDBConnected = false;
+var IsInserting = false;
+async function run() {
+	try {
+	  // Connect the client to the server	(optional starting in v4.7)
+	  let rs= await client.connect().then(function(data){
+		  //console.log("Connect :",data);
+	  })
+	  //console.log("Connect :",rs);
+	  //console.log("KQ isSuccess :",client.isSuccess);
+	  // Send a ping to confirm a successful connection
+	  //await client.db("ftechdb").command({ ping: 1 });
+	  database = client.db("ftechdb");
+	  
+	  try {
+		  //await client.db("ftechdb").command({ ping: 1 });
+		  database.command({ ping: 1 });
+		  console.log("Pinged your deployment. You successf");
+		  IsDBConnected=true;
+		  sensor_table = database.collection("sensor_table");
+		  let cuser =  sensor_table.find({
+		  	timestamp: {
+		  		$gt: new Date("2024-09-26T13:00:18Z"),
+		  		$lt: new Date("2024-10-01T23:58:18Z")
+		  	} 
+		  });
+		 // let cuser = sensor_table.find({});
+		  //console.log("List rows table sensor :",cuser);
+		  let listRS =[]
+		  await cuser.forEach(function(el){
+			  listRS.push(el);
+			  //console.log("el:",el);
+		  });
+		  console.log("listRS:",listRS);
+		  // listrow.forEach(element => {
+		  // 	console.log("forEach:",element);
+		  // });
+		  
+	  } catch (error) {
+		  IsDBConnected=false;
+		  console.log("Error:",error);
+	  }
+	  
+  
+	} finally {
+	  // Ensures that the client will close when you finish/error
+	  //await client.close();
+	}
+  }
+  
+//   setInterval( () => {
+	  
+//   	if(IsDBConnected && IsInserting==false && sensor_table!=null){
+//   	console.log("IsDBConnected:",IsDBConnected);
+//   	console.log("IsInserting:",IsInserting);
+//   	console.log("sensor_table:",(sensor_table!=null));
+//   		IsInserting=true;
+//   		const doc = {
+//   			machineserial:"5555",
+//   			temperature: 20.9,
+//   			humidity: 70,
+//   			timestamp:new Date()
+//   		  }
+		  
+//   		sensor_table.insertOne(doc).then(function(data){
+//   			console.log("Da them 1 dong:",doc);
+//   			IsInserting=false;
+//   		});
+		  
+//   	}
+//   }, 1000);
+  
+  run().catch(console.dir);
+
+  
+app.get('/insertdata', (req, res) => {
+	console.log("request",req.query);
+
+	if(IsDBConnected){
+		let machineserial = req.query.machineserial ;
+		let temperature = parseFloat(req.query.temperature) ;
+		let humidity 	= parseFloat(req.query.humidity) ; 
+		// check dữ liệu đầu vào
+	if (!machineserial || typeof temperature !== 'number' || typeof humidity !== 'number') {
+			return res.status(400).json({ error: 'machineserial, Temperature and humidity must be numbers' });
+	  }
+
+		IsInserting = true;
+		sensor_table.insertOne({machineserial:machineserial, temperature: temperature, humidity: humidity, timestamp: new Date() }).then(function (data) {
+			console.log("Da them 1 dong:", data);
+			IsInserting = false;
+			res.status(201).json(data);
+		});
+
+	}else{
+		return res.status(400).json({ error: 'Database is disconnected' });
+	}
+   
+})
+
+app.get('/datadate', async (req, res)  => {
+	let machineserial = req.query.machineserial ;
+	let fromDate = req.query.fromdate ;
+    	
+	if (!machineserial) {
+    		machineserial="5555";
+  	}
+	let fdate = toTimestamp(fromDate) || new Date();
+	fdate.setHours(0,0,0,0);
+	console.log("Lấy dữ liệu từ ngày: ",fdate)
+ 	// readDatabyDate(machineserial,toTimestamp(fdate), (data)=>{
+	// 	res.status(201).json(data);
+	// })
+
+	
+	let cuser =  sensor_table.find({
+		timestamp: {
+			$gt: fdate
+			//,$lt: new Date("2024-10-01T23:58:18Z")
+		},
+		machineserial: machineserial
+	});
+   // let cuser = sensor_table.find({});
+	//console.log("List rows table sensor :",cuser);
+	let listRS =[]
+	await cuser.forEach(function(el){
+		listRS.push(el);
+		console.log("el:",el);
+	});
+	console.log("listRS:",listRS);
+	res.status(201).json(listRS);
+})
